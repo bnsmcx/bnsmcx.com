e8a61228e0fbb31f5374f246c8a15f33c003c98f
ben <bnsmcx@protonmail.ch>
2021-05-08 17:23:57 +0700

    through lab 8 requirements for sdev300

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ PURPOSE:    Flask entry point.
 """
 import shutil
 from csv import DictReader, DictWriter
+from datetime import datetime
 from flask import Flask, render_template, request, redirect
 from passlib.hash import argon2
 
@@ -14,6 +15,7 @@ app = Flask(__name__)
 credentials = app.root_path + '/users.csv'
 temp_credentials = app.root_path + '/temp_users.csv'
 common_passwords = app.root_path + '/CommonPassword.txt'
+authentication_logfile = app.root_path + '/authentication.log'
 authenticated_user_sessions = {}
 
 
@@ -39,33 +41,107 @@ def blog():
 def login():
     """render the login page and process login attempts"""
     if request.method == 'POST':
+        user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
         username = request.form['username']
         password = request.form['password']
         if count_users() == 0:
             return render_template('login.html', error="User not found")
-        with open(credentials, 'r') as f:
-            reader = DictReader(f)
+        with open(credentials, 'r') as file:
+            reader = DictReader(file)
             for row in reader:
                 if username == row['username']:
                     if argon2.verify(password, row['password']):
                         response = redirect('/manage')
                         response.set_cookie('session_cookie', row['id'])
                         authenticated_user_sessions[row['id']] = username
+                        log_authentication_attempt(True, user_ip, username)
                         return response
-                    else:
-                        return render_template('login.html', error="Invalid Password")
+                    log_authentication_attempt(False, user_ip, username)
+                    return render_template('login.html', error="Invalid Password")
+            log_authentication_attempt(False, user_ip, username)
             return render_template('login.html', error="User not found")
     return render_template('login.html', error="")
 
 
+@app.route('/logout')
+def logout():
+    """logout the user"""
+    cookie = request.cookies.get('session_cookie')
+    if cookie in authenticated_user_sessions.keys():
+        authenticated_user_sessions.pop(cookie)
+    return redirect('/resume')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """accept user info, validate it, and create a new account"""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password != request.form['confirm_password']:
+            return render_template('register.html', error="Passwords don't match")
+        if username_available(username):
+            if not is_uncommon_password(password):
+                return render_template('register.html',
+                                       error="Your password is too common.")
+            if not is_complex_password(password):
+                return render_template('register.html',
+                                       error="""Insufficient password complexity,\n
+                                       it must be 12 characters and include at least 1 uppercase,
+                                        1 lowercase, 1 number and 1 special character""")
+            add_user(username, password)
+            return redirect('/login')
+        return render_template('register.html', error="Username not available")
+    return render_template('register.html', error="")
+
+
+@app.route('/change_pass', methods=['GET', 'POST'])
+def change_pass():
+    """change a user's password"""
+    if request.method == 'POST':
+        username = authenticated_user_sessions[request.cookies.get('session_cookie')]
+        password = request.form['password']
+        if user_is_authenticated() and password == request.form['confirm_password']:
+            if not is_uncommon_password(password):
+                return render_template('change_password.html',
+                                       error="Your password is too common.")
+            if not is_complex_password(password):
+                return render_template('change_password.html',
+                                       error="""Insufficient password complexity,\n
+                                       it must be 12 characters and include at least 1 uppercase,
+                                        1 lowercase, 1 number and 1 special character""")
+            set_password(username, password)
+            return redirect('/manage')
+        return render_template('change_password.html', error="Passwords don't match.")
+    return render_template('change_password.html', error='')
+
+
+@app.route('/manage')
+def management():
+    """force users to the login page if they aren't authenticated"""
+    cookie = request.cookies.get('session_cookie')
+    if user_is_authenticated():
+        user = authenticated_user_sessions[cookie]
+        return render_template('management.html', name=user)
+    return redirect('/login')
+
+
 def username_available(username: str) -> bool:
     """check if the username is available"""
-    with open(credentials, 'r') as f:
-        reader = DictReader(f)
+    with open(credentials, 'r') as file:
+        reader = DictReader(file)
         for row in reader:
             if row['username'] == username:
                 return False
     return True
+
+
+def log_authentication_attempt(successful: bool, user_ip: str, username: str):
+    """log an authentication attempt"""
+    outcome = 'SUCCESSFUL' if successful else 'FAILED'
+    with open(authentication_logfile, 'a') as file:
+        file.write(outcome + " login attempt for user " + username
+                   + " from " + user_ip + " at " + str(datetime.now()) + "\n")
 
 
 def is_complex_password(password: str) -> bool:
@@ -82,35 +158,11 @@ def is_complex_password(password: str) -> bool:
 def add_user(username, password):
     """add a new user record to users.csv"""
     password_hash = argon2.hash(password)
-    with open(credentials, 'a+') as f:
+    with open(credentials, 'a+') as file:
         next_id = count_users()
         if next_id > 0:
-            f.write('\n')
-        f.write('"' + username + '","' + password_hash + '",' + str(count_users()))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    """accept user info, validate it, and create a new account"""
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if password != request.form['confirm_password']:
-            return render_template('register.html', error="Passwords don't match")
-        if username_available(username):
-            if is_complex_password(password):
-                add_user(username, password)
-                return redirect('/login')
-            else:
-                return render_template('register.html',
-                                       error="""Insufficient password complexity, 
-                                                must be 12 characters and include 
-                                                at least 1 uppercase, 1 lowercase, 
-                                                1 number and 1 special character"""
-                                       )
-        else:
-            return render_template('register.html', error="Username not available")
-    return render_template('register.html', error="")
+            file.write('\n')
+        file.write('"' + username + '","' + password_hash + '",' + str(count_users()))
 
 
 def user_is_authenticated() -> bool:
@@ -124,9 +176,9 @@ def user_is_authenticated() -> bool:
 def is_uncommon_password(password):
     """check to see if a password is in a file of common passwords"""
     with open(common_passwords, 'r') as file:
-        for word in file:
-            if word == password:
-                return False
+        word_list = file.read()
+        if password in word_list:
+            return False
     return True
 
 
@@ -137,71 +189,31 @@ def _prep_temp_credentials_file():
         file.write("\n")
 
 
-def set_password(username: str, password: str) -> bool:
+def set_password(username: str, password: str):
     """set the password for a given username"""
-    if is_complex_password(password) and is_uncommon_password(password):
-        password_hash = argon2.hash(password)
-        _prep_temp_credentials_file()
-        with open(credentials, 'r') as file, open(temp_credentials, 'a') as temp_file:
-            reader = DictReader(file)
-            fieldnames = ['username', 'password', 'id']
-            writer = DictWriter(temp_file, fieldnames)
-            for row in reader:
-                if row['username'] == username:
-                    row['password'] = password_hash
-                    writer.writerow(row)
-                else:
-                    writer.writerow(row)
-        shutil.move(temp_credentials, credentials)
-        return True
-    return False
-
-
-@app.route('/change_pass', methods=['GET', 'POST'])
-def change_pass():
-    """change a user's password"""
-    if request.method == 'POST':
-        username = authenticated_user_sessions[request.cookies.get('session_cookie')]
-        password = request.form['password']
-        if user_is_authenticated() and password == request.form['confirm_password']:
-            if not set_password(username, password):
-                return render_template('change_password.html',
-                                       error="""Either your password is too common or used an insufficient
-                                        password complexity,\n it must be 12 characters and include 
-                                        at least 1 uppercase, 1 lowercase, 1 number and 1 special 
-                                        character""")
-            return redirect('/manage')
-        return render_template('change_password.html', error="Passwords don't match.")
-    return render_template('change_password.html', error='')
-
-
-@app.route('/manage')
-def management():
-    """force users to the login page if they aren't authenticated"""
-    cookie = request.cookies.get('session_cookie')
-    if user_is_authenticated():
-        user = authenticated_user_sessions[cookie]
-        return render_template('management.html', name=user)
-    return redirect('/login')
+    password_hash = argon2.hash(password)
+    _prep_temp_credentials_file()
+    with open(credentials, 'r') as file, open(temp_credentials, 'a') as temp_file:
+        reader = DictReader(file)
+        fieldnames = ['username', 'password', 'id']
+        writer = DictWriter(temp_file, fieldnames)
+        for row in reader:
+            if row['username'] == username:
+                row['password'] = password_hash
+                writer.writerow(row)
+            else:
+                writer.writerow(row)
+    shutil.move(temp_credentials, credentials)
 
 
 def count_users() -> int:
     """count the number of registered users"""
     users = 0
-    with open(credentials, 'r') as f:
-        reader = DictReader(f)
+    with open(credentials, 'r') as file:
+        reader = DictReader(file)
         for _ in reader:
             users = reader.line_num
     return users
-
-
-@app.route('/logout')
-def logout():
-    """logout the user"""
-    cookie = request.cookies.get('session_cookie')
-    if cookie in authenticated_user_sessions.keys():
-        authenticated_user_sessions.pop(cookie)
-    return redirect('/resume')
 
 
 if __name__ == '__main__':
