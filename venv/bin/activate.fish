6f277620a6f9dd61069265d782f801c105283119
ben <bnsmcx@protonmail.ch>
2021-06-14 13:50:02 +0700

    removed .idea and __pycache__, packed requirements.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ben/umgc/SDEV-300/wk6/lab6/venv"
+set -gx VIRTUAL_ENV "/home/ben/repos/tree_blog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
